61f93403c9468cb6c4fb513bb82fb07a69dfd5f1
lukelan <luke84320136@gmail.com>
2018-01-29 17:42:19 +0800

    Add keep-alive tag

--- src/components/New.vue
+++ src/components/New.vue
@@ -1,10 +1,20 @@
 <template>
   <div>
     <h3>新的記事</h3>
+    <button @click="counter++">新增</button>
+    <p>{{ counter }}</p>
   </div>
 </template>
 
 <script>
   export default {
+    data() {
+      return {
+        counter: 0
+      }
+    },
+    destroyed() {
+      console.log('Destrouyed')
+    }
   }
 </script>
